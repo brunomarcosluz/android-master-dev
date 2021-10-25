308404711ceccbb6251fd18dedb2171b2a215a1d
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-10-24 22:14:20 -0300

    commit

---
+++ Kotlin/fundamentos/4-funcoes.kt
@@ -0,0 +1,21 @@
+/*  funcoes em Kotlin sao declaradas a partir da palavra reservada 'fun' abreviacao de function
+ou para os bilingues 'diversao' em ingles XD */
+
+fun main()
+{ // isso é uma funcao
+
+    fun helloworld()
+    {
+        return println("Hello World")
+    }
+    
+
+    fun sum(a : Int, b : Int) 
+    { // isso é uma funcao
+        val soma = a + b
+        return println(soma)
+    }
+
+    sum(5,5)
+    helloworld()
+}
