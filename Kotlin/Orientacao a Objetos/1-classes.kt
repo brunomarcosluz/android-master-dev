66dca70fc047f1d09c72860dc16ad3a39fae56da
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-10-28 20:17:45 -0300

    commit

--- Kotlin/Orientacao a Objetos/1-classes.kt
+++ Kotlin/Orientacao a Objetos/1-classes.kt
@@ -3,9 +3,9 @@
 
 class Person { // note que não temos o "Public class, todas as classes são públicas por padrão em Kotlin"
     val name : String
-    val hair : String
-    val skin : String
-    val developer : Bollean 
+    var hair_type : String
+    val skin_tone : String
+    val isDeveloper : Bollean 
 }
 /* Criar classes em Kotlin é simples e não possui toda a verbalidade 
 do igual a de outras linguagens como o Java */
